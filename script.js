40a96f4994a696952b8cb931cec94ff41dc17662
Jesse Campbell <jesselcampbell@gmail.com>
2023-03-20 12:09:20 -0700

    Some pretty significant style updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { initPrismJS } from './prism'
+// import { initPrismJS } from './prism'
 
 const setActivePage = () => {
     const currentPage = document.querySelectorAll('a[href="' + window.location.pathname + '"]');
@@ -6,15 +6,11 @@ const setActivePage = () => {
         if (!page.classList.contains('super-navbar__logo') && !page.parentNode.classList.contains('notion-image')) {
             page.classList.add('page-active')
         }
-        const pageIcon = page.querySelector('.notion-page__icon')
-        if (pageIcon) {
-            pageIcon.setAttribute("style", "opacity:1!important; filter:grayscale(0%)!important;")
-        }
     });
 }
 
 window.addEventListener('load', e => {
-    initPrismJS()
+    // initPrismJS()
     setActivePage()
     next.router.events.on('routeChangeComplete', url => {
         // console.log(`Page has changed to ${url}`)
